3ac42c8bc7a0fad297bc17d79c463c8b04fdb952
VerifryingUname <kingdagone1@gmail.com>
2025-01-31 16:08:58 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,32 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double * data, int N, int M ){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(data + i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double * data, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        data[i] = (rand() % 101)/100.0;
+    }
+}
+void findRowSum(const double * data,double * sum, int N,int M){
+    for(int i = 0; i < N; i++){
+        sum[i] = 0;
+        for(int j = 0; j < M; j++){
+            sum[i] += data[i*M + j];
+        }
+    }
+}
+void findColSum(const double * data,double * sum,int N,int M){
+    for(int j; j < M; j++){
+        sum[j] = 0;
+        for(int i = 0; i < N; i++){
+            sum[j] += data[i*M + j];
+        }
+    }
+}
